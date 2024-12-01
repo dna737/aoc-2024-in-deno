382ba2c87b1f5c8c249c3814822340cc2d1a557a
dna737 <adnanhere27@gmail.com>
2024-12-01 00:11:58 -0600

    complete d1p1

---
+++ day-1/main.ts
@@ -0,0 +1,37 @@
+import fs from "node:fs";
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+function printCumulativeDifference(): void {
+  const data = fs.readFileSync("./input.txt", {
+    encoding: "utf-8",
+    flag: "r",
+  }).trim().split("\n");
+
+  const left: number[] = [];
+  const right: number[] = [];
+  let result: number = 0;
+
+  for (const line of data) {
+    const trimmed = line.trim().split(" ");
+    left.push(parseInt(trimmed[0]));
+    right.push(parseInt(trimmed[trimmed.length - 1]));
+  }
+
+  left.sort();
+  right.sort();
+
+  for (let i = 0; i < left.length; i++) {
+    result += Math.abs(left[i] - right[i]);
+  }
+
+  console.log("result:", result);
+}
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log("Add 2 + 3 =", add(2, 3));
+  printCumulativeDifference();
+}
