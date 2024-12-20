e87f577db2994f7be1ccdaa0882e56b5f22e60ec
dna737 <adnanhere27@gmail.com>
2024-12-20 10:26:52 -0600

    attempt to solve d2p2

--- day-2/main.ts
+++ day-2/main.ts
@@ -1,51 +1,97 @@
 import fs from "node:fs";
-import { stringify } from "node:querystring";
 
 export function add(a: number, b: number): number {
   return a + b;
 }
 
-function iterateLoop(levels: number[]): number {
-  let decreasing: number = 0;
-  let currDiff: number;
-
-    if(levels.length < 2) {
-      return 0;  //Test edge case later.
-    }
-
-  for(let i: number = 0; i < levels.length; i++) {
-
-    if(i === 0) {
-      decreasing = levels[i + 1] < levels[i] ? 1 : -1; //Useful for checking valid steps later.
-      continue;
-    }
-    
-    currDiff = -1 * decreasing * (levels[i] - levels[i - 1]);
-
-    if(currDiff < 1 || currDiff > 3) {
-      return 0;
-    } 
+export function getLoopMultiplier(levels: number[]): number {
+  const result: number[] = [];
+  
+  // Minimum of 3 comparisons required to find increasing or decreasing.
+  for (let i = 1; i < 4; i++) {
+    result.push(levels[i - 1] < levels[i] ? 1 : -1);
   }
 
+  return result.reduce((acc, x) => acc + x, 0) >= 1 ? -1 : 1;
+}
+
+export function getPredecessorIndex(
+  currIndex: number,
+  ignoredIndex: number | null,
+) {
+  if (ignoredIndex !== null && ignoredIndex === currIndex - 1) {
+    return currIndex - 2;
+  }
+
+  return currIndex - 1;
+}
+
+function iterateLoop(levels: number[], dampener?: boolean): number {
+  let currDiff: number;
+  let ignoredIndex: number | null = null; //Stores the index of the faulty number.
+  const decreasing: number = getLoopMultiplier(levels);
+  let couldBeFaulty: boolean = false;
+
+  if (levels.length < 2) {
+    return 0;
+  }
+
+  console.log("🚀 ~ iterateLoop ~ levels:", levels)
+  for (let i: number = 1; i < levels.length; i++) {
+
+  //Yet another invalid case.
+  if((levels[i] - levels[getPredecessorIndex(i, ignoredIndex)]) * -1 * (decreasing) <= 0) {
+    ignoredIndex = getPredecessorIndex(i, ignoredIndex);
+  }
+
+    currDiff = -1 * decreasing * (levels[i] - levels[getPredecessorIndex(i, ignoredIndex)]);
+    console.log("🚀 ~ iterateLoop ~ currDiff:", currDiff)
+
+    if (currDiff < 1 || currDiff > 3) {
+      if (!dampener) {
+        return 0;
+      }
+
+      if (ignoredIndex === null) {
+        ignoredIndex = i;
+      } else {
+        // consider 1 to be the upper limit
+        return 0;
+      }
+    }
+
+  }
   return 1;
 }
 
-export function printNumSafeReports(data: string[]): void {
-  let result = 0;
+export function printNumSafeReports(data: string[]): number {
+  let [result1, result2] = [0, 0];
+
   for (const line of data) {
     const levels: string[] = line.trim().split(" ");
-    result += iterateLoop(levels.map(x => parseInt(x)));
+    result1 += iterateLoop(levels.map((x) => parseInt(x)));
   }
-    console.log("🚀 ~ printNumSafeReports ~ result:", result)
+
+  for (const line of data) {
+    const levels: string[] = line.trim().split(" ");
+    result2 += iterateLoop(levels.map((x) => parseInt(x)), true);
+  }
+
+  console.log("🚀 ~ printNumSafeReports ~ result:", result1);
+  console.log("🚀 ~ printNumSafeReports ~ result2 :", result2);
+  return result2;
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-
   const data: string[] = fs.readFileSync("./input.txt", {
     encoding: "utf-8",
     flag: "r",
   }).trim().split("\n");
+
+  const mockData: string[] = [
+    "3 1 2 3 4 5"
+  ]
 
   printNumSafeReports(data);
 }
--- day-2/main_test.ts
+++ day-2/main_test.ts
@@ -1,6 +1,28 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { getLoopMultiplier, printNumSafeReports } from "./main.ts";
 
 Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+// assertEquals(printNumSafeReports([
+//   "9 7 6 2 1",
+// ]), 0);
+// assertEquals(printNumSafeReports([
+//   "7 6 4 2 1",
+// ]), 1);
+// assertEquals(printNumSafeReports([
+//   "1 2 7 8 9",
+// ]), 0);
+// assertEquals(printNumSafeReports([
+//   "1 3 2 4 5",
+// ]), 1);
+// assertEquals(printNumSafeReports([
+//   "8 6 4 4 1",
+// ]), 1);
+assertEquals(printNumSafeReports([
+"3 1 2 3 4 5"
+]), 1);
+
 });
+
+Deno.test(function testMultiplier() {
+  assertEquals(getLoopMultiplier([1, 2, 3]), -1) 
+})
