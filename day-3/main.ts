bd9fb91b3c6afc263f03dde44a22221ddb5ac5be
dna737 <adnanhere27@gmail.com>
2024-12-21 01:06:23 -0600

    complete d3p1

--- day-3/main.ts
+++ day-3/main.ts
@@ -1,10 +1,35 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import fs from "node:fs";
+
+export function parseMulValues(line: string): number {
+  const numRegex = /[0-9]+/g;
+  const totalMatches = [...line.matchAll(numRegex)];
+  const result: number[] = []; //Only 2 are required, but part 2 is very sneaky.
+
+  for (let i = 0; i < totalMatches.length; i++) {
+    result.push(parseInt(totalMatches[i][0] as string));
+  }
+
+  return result.reduce((acc, x) => acc * x, 1);
 }
 
-function 
+export function parseValidMuls(lines: string[]): number {
+  let result: number = 0;
+  const regex = /mul\([0-9]+,[0-9]+\)/g;
 
+  for(const line of lines) {
+    for(const occ of [...line.matchAll(regex)]) {
+      result += parseMulValues(occ[0]); //occ: [item, index, start, end, etc]. We're using item here.
+    }
+  }
+
+  return result;
+}
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const data: string[] = fs.readFileSync("./input.txt", {
+    encoding: "utf-8",
+    flag: "r",
+  }).trim().split("\n");
+
+  console.log("d3p1 result:", parseValidMuls(data));
 }
--- day-3/main_test.ts
+++ day-3/main_test.ts
@@ -1,6 +1,14 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { add,  parseValidMuls } from "./main.ts";
 
 Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+  const lines: string[] = [
+    "[%' mul(1,3){when()mul(1,0)@*mul(1,4)}?from()why()' do()@who()mul(1,6)](;/what()"
+  ]
+  assertEquals(parseValidMuls(lines), 13);
 });
+
+// Deno.test(function mulOperandsTest() {
+//   const line: string = "mul(1,3)"
+//   vfpassertEquals(parseMulOperands(line), [1, 3]);
+// });
