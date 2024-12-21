059b52c6e8c9f508f6f7a1d8c69d6b0f6c28e0af
dna737 <adnanhere27@gmail.com>
2024-12-21 09:11:56 -0600

    complete d3p2

--- day-3/main.ts
+++ day-3/main.ts
@@ -12,6 +12,7 @@ export function parseMulValues(line: string): number {
   return result.reduce((acc, x) => acc * x, 1);
 }
 
+//p1:
 export function parseValidMuls(lines: string[]): number {
   let result: number = 0;
   const regex = /mul\([0-9]+,[0-9]+\)/g;
@@ -24,6 +25,35 @@ export function parseValidMuls(lines: string[]): number {
 
   return result;
 }
+
+//p2:
+export function parseSelectiveMuls(lines: string[]): number {
+  let result: number = 0;
+  let shouldExecute: boolean = true
+  const regex = /mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)/g;
+
+  for(const line of lines) {
+    for(const occ of [...line.matchAll(regex)]) {
+      if(occ[0] === "do()") {
+        shouldExecute = true;
+        continue;
+      }
+
+      if(occ[0] === "don't()") {
+        shouldExecute = false;
+        continue;
+      }
+      
+      if(shouldExecute) {
+        result += parseMulValues(occ[0]); //occ: [item, index, start, end, etc]. We're using item here.
+      }
+      console.log("🚀 ~ parseSelectiveMuls ~ occ[0]:", occ[0])
+    }
+  }
+
+  return result; 
+}
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   const data: string[] = fs.readFileSync("./input.txt", {
@@ -32,4 +62,5 @@ if (import.meta.main) {
   }).trim().split("\n");
 
   console.log("d3p1 result:", parseValidMuls(data));
+  console.log("d3p2 result:", parseSelectiveMuls(data));
 }
